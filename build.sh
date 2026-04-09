c88414e06bd379cb328131e27ab1461c3d83107c
Tyler Liddicoat <liddicoattyler@gmail.com>
2026-04-09 08:07:02 -0500

    fix run

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # Build script with proper SDK configuration for macOS
 SDK=$(xcrun --sdk macosx --show-sdk-path)
-swift build -Xcc -isysroot -Xcc "$SDK" -Xcc -I -Xcc "$SDK/usr/include/c++/v1" "$@"
+
+# Check if first argument is "run" - if so, build first then run
+if [ "$1" = "run" ]; then
+    swift build -Xcc -isysroot -Xcc "$SDK" -Xcc -I -Xcc "$SDK/usr/include/c++/v1" && \
+    ./.build/debug/HockeyScoresAPI
+else
+    swift build -Xcc -isysroot -Xcc "$SDK" -Xcc -I -Xcc "$SDK/usr/include/c++/v1" "$@"
+fi
